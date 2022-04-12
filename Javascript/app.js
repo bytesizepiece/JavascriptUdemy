a8e0c7a1e12be2fabf994a74f54f3d0558382e28
Robin Gibbs <r.gibbs86@gmail.com>
2022-04-12 18:15:18 +0100

    update

---
+++ Javascript/app.js
@@ -0,0 +1,11 @@
+const firstName = "Robin";
+const lastName = "Shake&Bake";
+let fullName = firstName + " " + lastName
+
+console.log("helo there your full name is: " + firstName +" " + lastName);
+
+
+const website = "google";
+const url = "https//www." + website + ".com"
+
+console.log(url);
