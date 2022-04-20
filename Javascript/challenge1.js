17d3f97db90a1116305ea08e97ea2c8124ef5543
Robin Gibbs <r.gibbs86@gmail.com>
2022-04-20 10:58:13 +0100

    update

--- Javascript/challenge1.js
+++ Javascript/challenge1.js
@@ -1,22 +1,3 @@
-const score1 = 40;
-const score2 = 60;
-const score3 = 80;
+const fruits = ["banana", "apple", "orange", Math.floor(Math.random()*10 + 1) ]
 
-const total = score1 + score2 + score3;
-const average = total / 3
-
-console.log(total);
-console.log(average);
-
-const plates = 20
-const people = 7
-
-//calcs the remaining
-let remainPlates = plates % people
-
-//adds one to variable (increment)
-remainPlates++
-
-const message = "there are " + remainPlates + " plates available"
-
-console.log(message);
+console.log(fruits[3]);
