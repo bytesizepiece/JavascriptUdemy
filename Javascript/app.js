8d8aaa0aca2aefba306d68fec9d11fa414b1c2fc
Robin Gibbs <r.gibbs86@gmail.com>
2022-06-08 08:27:19 +0100

    update

--- Javascript/app.js
+++ Javascript/app.js
@@ -15,12 +15,3 @@ let person1 = {
     gender: "male",
     region: "local" 
 }
-=======
-let money = 0
-
-do {
-    console.log("you have " + money + " dollars");
-    money++
-}
-while (money < 10)
->>>>>>> 5f02a19461fc54b4be0a962a89baa890af69eec3
