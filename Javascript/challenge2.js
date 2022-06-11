99f73a621af8f23f03371d941233e113753b8a60
Robin Gibbs <r.gibbs86@gmail.com>
2022-06-11 07:37:21 +0100

    update

--- Javascript/challenge2.js
+++ Javascript/challenge2.js
@@ -4,7 +4,13 @@ Create a dice roll game
 drop down menu for number choice + submit
 
 
-- chose a number
+- chose a number  - 1 to 6
+- chose an amount to bet (start with 100)
 - roll the dice
 - is it a win, lose, error?
+- update "accoutn balance" + numRolls
 */
+
+
+let numRolls = 0
+document.getElementById("numRolls").innerHTML = numRolls;
