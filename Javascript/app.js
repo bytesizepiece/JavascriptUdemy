75654966ee6779214d97fbefa17867e0b4bd0c1b
Robin Gibbs <r.gibbs86@gmail.com>
2023-01-09 13:03:30 +0000

    update

---
+++ Javascript/app.js
@@ -0,0 +1,11 @@
+const gas = [20, 40, 100];
+
+const food = [10, 40, 50];
+
+function calculateTotal(arr){
+    let total = 0
+    for (let i = 0; i < arr.length; i++){
+        console.log(i);
+    }
+
+}
